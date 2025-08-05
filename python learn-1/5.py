1172e6801c1d3037cffd177c1e48f9829d7b9870
JEREMYVANS <1124112954@qq.com>
2025-08-05 22:45:27 +0800

    C5.2

--- python learn-1/5.py
+++ python learn-1/5.py
@@ -4,3 +4,22 @@ print(a,A)
 print(9/3)
 print(10//3)
 # //，称为地板除，两个整数的除法仍然是整数：
+
+#C5.2
+print(ord('A'))
+print(ord('中'))
+print(chr(66))
+print('\u4e2d\u6587')
+
+print(len('ABC'))
+
+r = 2.5
+s = 3.14 * r ** 2
+print(f'The area of a circle with radius {r} is {s:.2f}')
+
+s1 = 72
+s2 = 85
+r1 = (s2-s1)*100/(s1)
+print('Hello, {0}, 成绩提升了 {1:.1f}%'.format('小明', r1))
+
+#C5.3
